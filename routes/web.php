7b7e72646e92c59e48c537478d7521d500132ccf
MostafaElebzary <mostafaelebzary@gmail.com>
2023-03-26 00:44:02 +0200

    m

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(url('/nova'));
+    return redirect(url('/admin'));
 //    return view('welcome');
 });
